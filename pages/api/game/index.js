e49b1c75819e38e23a6b81b4efb6c7fbd98fa265
Max <OllieBate2007@outlook.com>
2025-10-21 23:12:03 +0100

    Update

--- pages/api/game/index.js
+++ pages/api/game/index.js
@@ -1,3 +1,10 @@
 export default async function handler(req, res) {
-    res.status(200).json({ message: "Game API endpoint" });
+    if (req.method !== 'GET') {
+        res.status(200).json({ message: "Game API endpoint" });
+    } else if (req.method === 'POST') {
+        res.status(200).json({ message: "Game API POST request received", data: req.body })
+    } else {
+        res.setHeader('Allow', ['GET', 'POST'])
+        res.status(405).end(`Method ${req.method} Not Allowed`)
+    };
 }
