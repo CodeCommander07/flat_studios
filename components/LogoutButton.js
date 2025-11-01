e04b3e1d50661c0eae6e3a1d6ab8b7d2a7b4fd42
Max <OllieBate2007@outlook.com>
2025-11-01 16:07:07 +0000

    update

--- components/LogoutButton.js
+++ components/LogoutButton.js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 
-export default function YourComponent() {
+export default function LogoutButton() {
   const router = useRouter();
 
   const handleLogout = () => {
