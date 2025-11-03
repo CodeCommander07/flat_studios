9967dea2c7af68ff8b79055877ade01ea40f5292
Max <OllieBate2007@outlook.com>
2025-11-03 20:29:57 +0000

    aa

--- pages/api/developers/search.js
+++ pages/api/developers/search.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         { userId: { $regex: q, $options: 'i' } },
       ],
     })
-      .select('discordUsername robloxUsername defaultAvatar userId')
+      .select('email username discordUsername robloxUsername defaultAvatar userId')
       .limit(10);
 
     return res.status(200).json(users);
