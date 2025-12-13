5e8c3ad74ed65706782a0938570e877a824799ee
Max <OllieBate2007@outlook.com>
2025-12-13 00:28:32 +0000

    a

--- pages/me/index.js
+++ pages/me/index.js
@@ -755,7 +755,7 @@ export default function ProfilePage() {
                       <h2 className="text-xl font-semibold pb-6">
                         Connections
                       </h2>
-                      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 lg:col-span-2">
+                      <div className="grid grid-cols-1 gap-6 lg:col-span-2">
                         {[
                           {
                             name: 'Discord',
